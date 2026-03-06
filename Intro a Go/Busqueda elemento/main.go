68574612cd7800d12eb494aaca2f90b67778b3a0
mirkopresa <mirkocamilopresa@proton.me>
2026-03-06 21:36:22 +0000

    First commit

---
+++ Intro a Go/Busqueda elemento/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func BuscarElem(arr []int, elem int) int {
+	// for index, value := range arr {
+	// 	if elem == value {
+	//		return index
+	// 	}
+	// }
+
+	for i := 0; i < len(arr); i++ {
+		if elem == arr[i] {
+			return i
+		}
+	}
+	return -1
+}
+
+func main() {
+	array := []int{20, 50, 4, 77}
+	fmt.Println(BuscarElem(array, 7))
+}
