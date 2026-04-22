2a1d4430ac42d54cefc16780602c2804cd4f3f36
mirkopresa <mirkocamilopresa@proton.me>
2026-04-22 22:35:03 +0000

    Ejercicio 3 resuelto

---
+++ Ejercicios/Practica parciales/p1-2025c2-r1/03 - Pila sumar/main.go
@@ -0,0 +1,17 @@
+// Implementar una función que reciba una pila de enteros y devuelva la suma de todos los elementos. Al finalizar la
+// ejecución de la función, la pila debe quedar en el mismo estado que tenía antes de ejecutar la misma. La función no puede
+// utilizar estructuras auxiliares (incluyendo arreglos). La firma de la función debe ser pilaSumar(pila Pila[int]) int.
+// Indicar y justificar la complejidad de la función implementada
+
+package main
+
+// O(n), siendo n la cantidad de elementos a desapilar y sumar, por ende la cantidad de llamados recursivos
+func pilaSumar(pila Pila[int]) int {
+	if pila.EstaVacia() {
+		return 0
+	}
+	desapilado := pila.Desapilar()
+	suma := desapilado + pilaSumar(pila)
+	pila.Apilar(desapilado)
+	return suma
+}
