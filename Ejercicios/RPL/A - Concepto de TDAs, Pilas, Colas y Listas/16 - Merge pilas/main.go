bfffef984e60b3daeafcf88bb3de92bfebe5c9de
mirkopresa <mirkocamilopresa@proton.me>
2026-04-26 18:19:25 +0000

    Comento una manera de hacerlo y agrego otra con pilaAux

--- Ejercicios/RPL/A - Concepto de TDAs, Pilas, Colas y Listas/16 - Merge pilas/main.go
+++ Ejercicios/RPL/A - Concepto de TDAs, Pilas, Colas y Listas/16 - Merge pilas/main.go
@@ -6,37 +6,68 @@
 
 package main
 
-func cargarDesapiladosNoRepetidos(arr []int, pila Pila[int]) []int {
-	desapilado := pila.Desapilar()
-	if len(arr) == 0 || arr[len(arr)-1] != desapilado {
-		arr = append(arr, desapilado)
-	}
-	return arr
-}
+// Version con arreglo
+// func cargarDesapiladosNoRepetidos(arr []int, pila Pila[int]) []int {
+// 	desapilado := pila.Desapilar()
+// 	if len(arr) == 0 || arr[len(arr)-1] != desapilado {
+// 		arr = append(arr, desapilado)
+// 	}
+// 	return arr
+// }
 
-func invertirArreglo(arr []int) []int {
-	i, j := len(arr)-1, 0
-	for i > j {
-		arr[i], arr[j] = arr[j], arr[i]
-		i--
-		j++
+// func invertirArreglo(arr []int) []int {
+// 	i, j := len(arr)-1, 0
+// 	for i > j {
+// 		arr[i], arr[j] = arr[j], arr[i]
+// 		i--
+// 		j++
+// 	}
+// 	return arr
+// }
+
+// func MergePilas(pila1, pila2 Pila[int]) []int {
+// 	arr := make([]int, 0, 0)
+// 	for !pila1.EstaVacia() || !pila2.EstaVacia() {
+// 		if !pila1.EstaVacia() && !pila2.EstaVacia() && (pila1.VerTope() < pila2.VerTope()) {
+// 			arr = cargarDesapiladosNoRepetidos(arr, pila2)
+// 		} else if !pila1.EstaVacia() && !pila2.EstaVacia() && (pila1.VerTope() > pila2.VerTope()) {
+// 			arr = cargarDesapiladosNoRepetidos(arr, pila1)
+// 		} else if pila1.EstaVacia() {
+// 			arr = cargarDesapiladosNoRepetidos(arr, pila2)
+// 		} else {
+// 			arr = cargarDesapiladosNoRepetidos(arr, pila1)
+// 		}
+// 	}
+// 	arr = invertirArreglo(arr)
+// 	return arr
+// }
+
+func ApilarSinRepetir(pila Pila[int], desapilado int) {
+	if pila.EstaVacia() || pila.VerTope() != desapilado {
+		pila.Apilar(desapilado)
 	}
-	return arr
 }
 
 func MergePilas(pila1, pila2 Pila[int]) []int {
-	arr := make([]int, 0, 0)
-	for !pila1.EstaVacia() || !pila2.EstaVacia() {
-		if !pila1.EstaVacia() && !pila2.EstaVacia() && (pila1.VerTope() < pila2.VerTope()) {
-			arr = cargarDesapiladosNoRepetidos(arr, pila2)
-		} else if !pila1.EstaVacia() && !pila2.EstaVacia() && (pila1.VerTope() > pila2.VerTope()) {
-			arr = cargarDesapiladosNoRepetidos(arr, pila1)
-		} else if pila1.EstaVacia() {
-			arr = cargarDesapiladosNoRepetidos(arr, pila2)
+	pilaAux := CrearPilaDinamica[int]()
+	for !pila1.EstaVacia() && !pila2.EstaVacia() {
+		tope1 := pila1.VerTope()
+		tope2 := pila2.VerTope()
+		if tope1 > tope2 {
+			ApilarSinRepetir(pilaAux, pila1.Desapilar())
 		} else {
-			arr = cargarDesapiladosNoRepetidos(arr, pila1)
+			ApilarSinRepetir(pilaAux, pila2.Desapilar())
 		}
 	}
-	arr = invertirArreglo(arr)
-	return arr
+	for !pila1.EstaVacia() {
+		ApilarSinRepetir(pilaAux, pila1.Desapilar())
+	}
+	for !pila2.EstaVacia() {
+		ApilarSinRepetir(pilaAux, pila2.Desapilar())
+	}
+	resultado := make([]int, 0)
+	for !pilaAux.EstaVacia() {
+		resultado = append(resultado, pilaAux.Desapilar())
+	}
+	return resultado
 }
