c00eea98bc19dbf216503bbacffa7fc5b8cd66d1
mirkopresa <mirkocamilopresa@proton.me>
2026-06-11 16:20:35 +0000

    Ejercicios de grafos resueltos

---
+++ Ejercicios/RPL/G - Grafos/Python/21-ecosistema.py
@@ -0,0 +1,39 @@
+# Contamos con un grafo dirigido que modela un ecosistema. En dicho grafo, cada vértice es una especie, y cada arista (v, w) 
+# indica que v es depredador natural de w. Considerando la horrible tendencia del ser humano por llevar a la extinción especies, 
+# algo que nos puede interesar es saber si existe alguna especie que, si llegara a desaparecer, rompería todo el ecosistema: 
+# quienes la depredan no tienen un sustituto (y, por ende, pueden desaparecer también) y/o quienes eran depredados por esta ya no tienen amenazas, 
+# por lo que crecerán descontroladamente. 
+# Implementar un algoritmo que reciba un grafo de dichas características y devuelva una lista de todas las 
+# especies que cumplan lo antes mencionado. Indicar y justificar la complejidad del algoritmo implementado.
+
+def amenazados(grafo):
+    'Devolver una lista con los vértices que cumplen la condición'
+    grados_e = grados_entrada(grafo)
+    grados_s = grados_salida(grafo)
+    resultado = []
+    especies = set()
+    for v in grafo:
+        for w in grafo.adyacentes(v):
+            if grados_e[w] == 1 and v not in especies:
+                resultado.append(v)
+                especies.add(v)
+        if grados_s[v] == 1:
+            for w in grafo.adyacentes(v):
+                if w not in especies:
+                    resultado.append(w)
+                    especies.add(w)
+    return resultado
+
+def grados_entrada(grafo):
+    grados = {}
+    for v in grafo: grados[v] = 0
+    for v in grafo:
+        for w in grafo.adyacentes(v):
+            grados[w] += 1
+    return grados
+
+def grados_salida(grafo):
+    grados = {}
+    for v in grafo:
+        grados[v] = len(grafo.adyacentes(v))
+    return grados
