867fd4dc510593dcf2d0e2a1ab119a83188b3a34
mirkopresa <mirkocamilopresa@proton.me>
2026-06-16 17:37:26 +0000

    Agrego resolucion con counting sort

--- Ejercicios/Practica parciales/Primer parcialito 2025/p1-2025c2/03 - Mas grande posible/main.go
+++ Ejercicios/Practica parciales/Primer parcialito 2025/p1-2025c2/03 - Mas grande posible/main.go
@@ -45,7 +45,37 @@ func masGrandePosible(digitos []int) int {
 	return resultado
 }
 
+// Hagamoslo con counting sort, para hacerlo O(n)
+
+func CountingSort(arreglo []int, k int) []int {
+	frecuencias := make([]int, k)
+	for _, elem := range arreglo {
+		frecuencias[elem]++
+	}
+	sumasAcumuladas := make([]int, k)
+	for i := 1; i < k; i++ {
+		sumasAcumuladas[i] = sumasAcumuladas[i-1] + frecuencias[i-1]
+	}
+	resultado := make([]int, len(arreglo))
+	for _, elem := range arreglo {
+		resultado[sumasAcumuladas[elem]] = elem
+		sumasAcumuladas[elem]++
+	}
+	return resultado
+}
+
+func masGrandePosible2(digitos []int) int {
+	digitos = CountingSort(digitos, 10)
+	resultado := 0
+	// O(n) siendo n la cantidad de digitos
+	for i := len(digitos) - 1; i >= 0; i-- {
+		resultado = resultado*10 + digitos[i]
+	}
+	return resultado
+}
 func main() {
 	digitos := []int{3, 1, 4, 1, 5, 9}
 	println(masGrandePosible(digitos))
+	digitos2 := []int{5, 4, 0, 9, 1, 0, 0, 0, 0, 1, 2, 8}
+	println(masGrandePosible2(digitos2))
 }
